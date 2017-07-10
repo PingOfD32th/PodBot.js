fbe1740eb89fb7d187951e83920669b105d9b96b
PingOfD32th <gkgleason320@gmail.com>
2017-07-10 11:45:42 -0500

    updates
    updated https to correct http
    added rules

--- PodBot.js
+++ PodBot.js
@@ -25,26 +25,26 @@ client.on('guildMemberAdd', member => {
 const data1 = {
   "embed": {
     "title": "PoolofD32th.club",
-    "description": "Reward Recipient: [BURST-E925-FACX-C2X8-49772](http://burstcoin.biz/address/2311656582822632451)\n First brought online: [04/12/17](https://poolofd32th.club)\n Payout: [300 Mined coin OR 24 hrs](https://beta.poolofd32th.club/pools/MainPayoutFAQ.html)\n Rewards: [60/40 (standard)](https://beta.poolofd32th.club/pools/MainPayoutFAQ.html)\n Block History: [50 (standard)](https://beta.poolofd32th.club/pools/MainPayoutFAQ.html)\n Recommended plot size: [5TB+](https://beta.poolofd32th.club/pools/MainPayoutFAQ.html)\n Highest deadline accepted: [14 days (1209600)](https://beta.poolofd32th.club/pools/MainPayoutFAQ.html) \n",
-    "url": "https://poolofd32th.club",
+    "description": "Reward Recipient: [BURST-E925-FACX-C2X8-49772](http://burstcoin.biz/address/2311656582822632451)\n First brought online: [04/12/17](http://poolofd32th.club)\n Payout: [300 Mined coin OR 24 hrs](http://beta.poolofd32th.club/pools/MainPayoutFAQ.html)\n Rewards: [60/40 (standard)](http://beta.poolofd32th.club/pools/MainPayoutFAQ.html)\n Block History: [50 (standard)](http://beta.poolofd32th.club/pools/MainPayoutFAQ.html)\n Recommended plot size: [5TB+](http://beta.poolofd32th.club/pools/MainPayoutFAQ.html)\n Highest deadline accepted: [14 days (1209600)](http://beta.poolofd32th.club/pools/MainPayoutFAQ.html) \n",
+    "url": "http://poolofd32th.club",
     "color": 7976557,
     "author": {
       "name": "Mother Pool Info",
-      "url": "https://poolofd32th.club",
-      "icon_url": "https://forums.burst-team.us/assets/uploads/profile/3216-profileimg.png"
+      "url": "http://poolofd32th.club",
+      "icon_url": "http://forums.burst-team.us/assets/uploads/profile/3216-profileimg.png"
     }
   }
 };
 const data2 = {
   "embed": {
     "title": "xen.poolofD32th.club",
-    "description": "Reward Recipient: [BURST-LBQ2-XLPT-S2S8-64ZG5](http://burstcoin.biz/address/4736628939229308608)\n First brought online: [06/11/17](https://xen.poolofd32th.club)\n Fee: [1% (18.4 Burst/Blockfind)](https://xen.poolofd32th.club/js/test.html)\n Payout: [200 Mined coin OR 48 hrs](https://xen.poolofd32th.club/js/test.html)\n Rewards: [20/80](http://xen.poolofd32th.club/js/test.html)\n Block History: [500](http://xen.poolofd32th.club/js/test.html)\n Recommended plot size: [300GB+](http://xen.poolofd32th.club/js/test.html)\n Highest deadline accepted: [365 days (31536000)](http://xen.poolofd32th.club/js/test.html)\n",
-    "url": "https://xen.poolofd32th.club",
+    "description": "Reward Recipient: [BURST-LBQ2-XLPT-S2S8-64ZG5](http://burstcoin.biz/address/4736628939229308608)\n First brought online: [06/11/17](http://xen.poolofd32th.club)\n Fee: [1% (18.4 Burst/Blockfind)](http://xen.poolofd32th.club/js/test.html)\n Payout: [200 Mined coin OR 48 hrs](http://xen.poolofd32th.club/js/test.html)\n Rewards: [20/80](http://xen.poolofd32th.club/js/test.html)\n Block History: [500](http://xen.poolofd32th.club/js/test.html)\n Recommended plot size: [300GB+](http://xen.poolofd32th.club/js/test.html)\n Highest deadline accepted: [365 days (31536000)](http://xen.poolofd32th.club/js/test.html)\n",
+    "url": "http://xen.poolofd32th.club",
     "color": 7976557,
     "author": {
       "name": "Daughter Pool Info",
-      "url": "https://xen.poolofd32th.club",
-      "icon_url": "https://forums.burst-team.us/assets/uploads/profile/3216-profileimg.png"
+      "url": "http://xen.poolofd32th.club",
+      "icon_url": "http://forums.burst-team.us/assets/uploads/profile/3216-profileimg.png"
     }
   }
 };
@@ -55,19 +55,19 @@ const data3 = {
     "color": 7976557,
     "author": {
       "name": "Faucet",
-      "icon_url": "https://forums.burst-team.us/assets/uploads/profile/3216-profileimg.png"
+      "icon_url": "http://forums.burst-team.us/assets/uploads/profile/3216-profileimg.png"
     }
   }
 };
 const data4 = {
   "embed": {
-    "description": "Pool server is owned by: Gregory Gleason [@PingOfD32th#4641](https://forums.burst-team.us/user/pingofd3ath)\nBrains of the server is: God [@manfromafar#2786](https://forums.burst-team.us/user/manfromafar) \n",
-    "url": "https://poolofd32th.club",
+    "description": "Pool server is owned by: Gregory Gleason [@PingOfD32th#4641](http://forums.burst-team.us/user/pingofd3ath)\nBrains of the server is: God [@manfromafar#2786](http://forums.burst-team.us/user/manfromafar) \n",
+    "url": "http://poolofd32th.club",
     "color": 7976557,
     "author": {
       "name": "Owner Info",
-      "url": "https://poolofd32th.club",
-      "icon_url": "https://forums.burst-team.us/assets/uploads/profile/3216-profileimg.png"
+      "url": "http://poolofd32th.club",
+      "icon_url": "http://forums.burst-team.us/assets/uploads/profile/3216-profileimg.png"
     }
   }
 };
@@ -86,6 +86,9 @@ if (!message.content.startsWith(config.prefi)) return;
   }else {
     if (message.content === config.prefix + 'daughter') {
       message.channel.send(data2);
+    }else{
+      if (message.content === config.prefix + 'xen') {
+      message.channel.send(data2);
     }else {
       if (message.content === config.prefix + 'main') {
         message.channel.send(data1);
@@ -98,19 +101,26 @@ if (!message.content.startsWith(config.prefi)) return;
       }else {
         if (message.content === config.prefix + 'banner') {
           message.channel.send('Possible Banner Background by **Smiley =)**', {
-            file: "https://cdn.discordapp.com/attachments/319637612019318784/326599023588933633/unknown.png"
+            file: "http://cdn.discordapp.com/attachments/319637612019318784/326599023588933633/unknown.png"
           })
         }else {
           if (message.content === config.prefix + 'uptime') {
             message.channel.send('current uptime is ' + Math.floor(process.uptime()/60) + ' minutes.');
           }
         }
+        }
       }
   }
 }
 }
 }});
-
+//rules
+client.on('message', message => {
+  if (message.content === config.prefix + 'rules') {
+    message.channel.send("Rules:\n1) Respect everyone, & if a mod+ tells you not to do something. DON'T.\n2) Help those you can, we were all noobs at some point.\n3) Don't attempt to abuse the faucet. I will ban you.\n4) Don't make permanent share links. These can be used to abuse faucet.\n5) Don't be an ass\n6) Don't ask if you can ask a question, ask and if someone knows, they will answer.");
+    console.log('Rules Requested');
+  }
+});
 //eval
 function clean(text) {
   if (typeof(text) === "string")
@@ -143,3 +153,4 @@ client.login(config.token);
 //to add
 //rules list
 //commands list
+//payout rules
